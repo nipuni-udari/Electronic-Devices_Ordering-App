eb3f3e26c4640674f603eaf787387c061d465444
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-11-20 17:03:22 +0530

    update style file

--- app/global/styles.js
+++ app/global/styles.js
@@ -13,4 +13,30 @@ export const colors = {
 
 export const parameters = {
   headerHeight: 40,
+
+  buttonTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    allignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -3,
+  },
+
+  styleButton: {
+    backgroundColor: '#ff8c52',
+    allignContent: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: '#ff8c52',
+    height: 50,
+    paddingHorizontal: 20,
+    width: '100%',
+  },
+};
+export const title = {
+  color: '#ff8c52',
+  fontSize: 20,
+  fontWeight: 'bold',
 };
