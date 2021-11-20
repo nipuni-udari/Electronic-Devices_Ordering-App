b75a201ca574d832bc8721fc4ecb1b32e4df1306
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-11-20 20:13:24 +0530

    Add third image swiper

--- app/screens/authScreens/SignInWelcomeScreen.js
+++ app/screens/authScreens/SignInWelcomeScreen.js
@@ -45,6 +45,12 @@ export default function SignInWelcomeScreen({navigation}) {
               style={{height: '100%', width: '100%'}}
             />
           </View>
+          <View style={styles.slide3}>
+            <Image
+              source={require('../../assets/images/3.png')}
+              style={{height: '100%', width: '100%'}}
+            />
+          </View>
     </View>
   );
 }
