85e3c138de6754368ac5e02677c9e2f9f5c65ebf
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-11-20 20:09:59 +0530

    add welcomeSignInScreen for Root

--- app/Root.js
+++ app/Root.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {View, Text, StyleSheet, StatusBar} from 'react-native';
 import {colors} from './global/styles';
 import SignInScreen from './screens/authScreens/SignInScreen';
+import SignInWelcomeScreen from './screens/authScreens/SignInWelcomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={colors.statusbar} />
-      <SignInScreen />
+      <SignInWelcomeScreen />
     </View>
   );
 }
