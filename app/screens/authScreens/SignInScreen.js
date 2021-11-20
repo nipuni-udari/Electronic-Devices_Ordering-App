6a18cf14879307bc0819fa7d2d10705668830576
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-11-20 18:00:32 +0530

    create sign in texts

--- app/screens/authScreens/SignInScreen.js
+++ app/screens/authScreens/SignInScreen.js
@@ -4,75 +4,78 @@ import {colors, parameters} from '../../global/styles';
 import {Icon} from 'react-native-elements';
 import Header from '../../components/Header';
 
-export default function SignInScreen(){
+export default function SignInScreen() {
+  return (
+    <View style={styles.container}>
+      <Header title="MY ACCOUNT" type="arrow-left" navigation={navigation} />
 
-    return (
-      <View style={styles.container}>
-          <Header title="MY ACCOUNT" type="arrow-left" />
+      <View style={{marginLeft: 20, marginTop: 10}}>
+        <Text style={title}>Sign-In</Text>
+      </View>
 
-
-
-        </View>
-    )
+      <View style={{alignItems: 'center', marginTop: 10}}>
+        <Text style={styles.text1}>Please enter the email and password</Text>
+        <Text style={styles.text1}>registered with your account</Text>
+      </View>
+    </View>
+  );
 }
 
-
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-  
-    text1: {
-      color: colors.grey3,
-      fontSize: 16,
-    },
-  
-    TextInput1: {
-      borderWidth: 1,
-      borderColor: '#86939e',
-      marginHorizontal: 20,
-      borderRadius: 12,
-      marginBottom: 20,
-      paddingLeft: 15,
-      paddingTop: 12,
-    },
-  
-    TextInput2: {
-      borderWidth: 1,
-      borderColor: '#86939e',
-      marginHorizontal: 20,
-      borderRadius: 12,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      allignContent: 'center',
-      allignItems: 'center',
-      paddingTop: 4,
-      paddingLeft: 5,
-    },
-  
-    SocialIcon: {
-      borderRadius: 10,
-      height: 50,
-    },
-  
-    createButton: {
-      borderWidth: 1,
-      borderColor: '#ff8c52',
-      height: 40,
-      borderRadius: 12,
-      backgroundColor: 'white',
-      justifyContent: 'center',
-      allignContent: 'center',
-      paddingHorizontal: 20,
-    },
-  
-    createButtonTitle: {
-      color: '#ff8c52',
-      fontSize: 16,
-      fontWeight: 'bold',
-      allignItems: 'center',
-      justifyContent: 'center',
-      marginTop: -3,
-    },
-  });
+  container: {
+    flex: 1,
+  },
+
+  text1: {
+    color: colors.grey3,
+    fontSize: 16,
+  },
+
+  TextInput1: {
+    borderWidth: 1,
+    borderColor: '#86939e',
+    marginHorizontal: 20,
+    borderRadius: 12,
+    marginBottom: 20,
+    paddingLeft: 15,
+    paddingTop: 12,
+  },
+
+  TextInput2: {
+    borderWidth: 1,
+    borderColor: '#86939e',
+    marginHorizontal: 20,
+    borderRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    allignContent: 'center',
+    allignItems: 'center',
+    paddingTop: 4,
+    paddingLeft: 5,
+  },
+
+  SocialIcon: {
+    borderRadius: 10,
+    height: 50,
+  },
+
+  createButton: {
+    borderWidth: 1,
+    borderColor: '#ff8c52',
+    height: 40,
+    borderRadius: 12,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    allignContent: 'center',
+    paddingHorizontal: 20,
+  },
+
+  createButtonTitle: {
+    color: '#ff8c52',
+    fontSize: 16,
+    fontWeight: 'bold',
+    allignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -3,
+  },
+});
