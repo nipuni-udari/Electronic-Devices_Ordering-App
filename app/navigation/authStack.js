72bf58571a3f0093125d84e8d6dbe9a1a6698d03
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-11-22 08:09:23 +0530

    change the HomeScreen path

--- app/navigation/authStack.js
+++ app/navigation/authStack.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import SignInWelcomeScreen from '../screens/authScreens/SignInWelcomeScreen';
 import SignInScreen from '../screens/authScreens/SignInScreen';
-import HomeScreen from '../screens/authScreens/HomeScreen';
+import HomeScreen from '../screens/HomeScreen';
 
 const Auth = createStackNavigator();
 
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -14,6 +14,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center,',
+    alignItems: 'center',
   },
 });
