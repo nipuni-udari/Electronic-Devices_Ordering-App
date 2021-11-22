1f07dd71065c7c83d157734239548ab2e4bd89d9
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-11-22 09:04:45 +0530

    create HomeScreen header menu icon with text

--- app/components/HomeHeader.js
+++ app/components/HomeHeader.js
@@ -4,21 +4,45 @@ import {View, Text, StyleSheet} from 'react-native';
 import {Icon, withBadge} from 'react-native-elements';
 import {colors, parameters} from '../global/styles';
 
-export default function HomeHeader({}){
-    return(
-        <View style={StyleSheet.header}>
+export default function HomeHeader() {
+  const BadgeIcon = withBadge(0)(Icon);
+  return (
+    <View style={styles.header}>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginLeft: 15,
+        }}>
+        <Icon
+          type="material-community"
+          name="menu"
+          color={colors.cardbackground}
+          size={32}
+        />
+      </View>
 
-        </View>
-    );
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <Text
+          style={{
+            color: colors.cardbackground,
+            fontSize: 25,
+            fontWeight: 'bold',
+          }}>
+          Xpress E-Tech
+        </Text>
+      </View>
+
+      
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-
-    header:{
-        flexDirection:'row',
-        backgroundColor:colors.buttons,
-        height:parameters.headerHeight,
-        justifyContent:'space-between'
-    }
+  header: {
+    flexDirection: 'row',
+    backgroundColor: colors.buttons,
+    height: parameters.headerHeight,
+    justifyContent: 'space-between',
+  },
 });
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import {View, Text, StyleSheet} from 'react-native';
 import {Icon} from 'react-native-elements';
+import HomeHeader from '../components/HomeHeader';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      
+      <HomeHeader />
     </View>
   );
 }
@@ -14,6 +15,5 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
   },
 });
