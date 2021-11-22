9ef0860348be40f1e77ccd6e04051368be77acbd
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-11-22 10:41:00 +0530

    create delivery and pickUp Buttons for HomeScreen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  Pressable,
+  Image,
+  Dimensions,
+  StatusBar,
+} from 'react-native';
 import {Icon} from 'react-native-elements';
+import {colors, paremeters} from '../global/styles';
 import HomeHeader from '../components/HomeHeader';
 
 export default function HomeScreen() {
+  const [delivery, setDelivery] = useState(true);
   return (
     <View style={styles.container}>
       <HomeHeader />
+      <View
+        style={{
+          marginTop: 10,
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+        }}>
+        <TouchableOpacity>
+          <View
+            style={{
+              ...styles.deliveryButton,
+              backgroundColor: delivery ? colors.buttons : colors.grey5,
+            }}>
+            <Text style={styles.deliveryText}>Delivery</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <View
+            style={{
+              ...styles.deliveryButton,
+              backgroundColor: delivery ? colors.grey5 : colors.buttons,
+            }}>
+            <Text style={styles.deliveryText}>Pick Up</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -15,5 +54,97 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 20,
+  },
+  deliveryButton: {
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    paddingVertical: 5,
+  },
+
+  deliveryText: {
+    marginLeft: 5,
+    fontSize: 16,
+  },
+
+  filterView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+
+  clockView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+    backgroundColor: colors.cardbackground,
+    borderRadius: 15,
+    paddingHorizontal: 5,
+    marginRight: 20,
+  },
+  addressView: {
+    flexDirection: 'row',
+    backgroundColor: colors.grey5,
+    borderRadius: 15,
+    paddingVertical: 3,
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+
+  headerText: {
+    color: colors.grey2,
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingLeft: 10,
+  },
+  headerTextView: {
+    backgroundColor: colors.grey5,
+    paddingVertical: 3,
+  },
+
+  smallCard: {
+    borderRadius: 30,
+    backgroundColor: colors.grey5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    width: 80,
+    margin: 10,
+    height: 100,
+  },
+
+  smallCardSelected: {
+    borderRadius: 30,
+    backgroundColor: colors.buttons,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    width: 80,
+    margin: 10,
+    height: 100,
+  },
+
+  smallCardTextSected: {
+    fontWeight: 'bold',
+    color: colors.cardbackground,
+  },
+
+  smallCardText: {
+    fontWeight: 'bold',
+    color: colors.grey2,
+  },
+
+  floatButton: {
+    position: 'absolute',
+    bottom: 10,
+    right: 15,
+    backgroundColor: 'white',
+    elevation: 10,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    alignItems: 'center',
   },
 });
