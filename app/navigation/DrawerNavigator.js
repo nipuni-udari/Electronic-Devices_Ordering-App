65325cc3d0b0b96c1eb4871a86d865728cc357a1
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-11-24 10:36:49 +0530

    create drawer navigator content items

--- app/navigation/DrawerNavigator.js
+++ app/navigation/DrawerNavigator.js
@@ -5,12 +5,13 @@ import RootClientTabs from './ClientTabs';
 import {Icon} from 'react-native-elements';
 import {colors} from '../global/styles';
 import BusinessConsoleScreen from '../screens/BusinessConsoleScreen';
+import DrawerContent from '../components/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
       <Drawer.Screen
         name="RootClientTabs"
         component={RootClientTabs}
