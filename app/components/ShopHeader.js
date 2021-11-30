34acb881ea5fd80f2d5f9d4686415d7ecc35976f
nipuni-udari <96770@fhss.sjp.ac.lk>
2021-11-30 20:28:27 +0530

    Create animation of the Heart

--- app/components/ShopHeader.js
+++ app/components/ShopHeader.js
@@ -7,10 +7,39 @@ import {Icon} from 'react-native-elements';
 
 export default function ShopHeader({navigation, id}) {
   const index2 = 10;
+  const currentValue = new Animated.Value(1);
 
   const [liked, setLiked] = useState(false);
   const [counter, setCounter] = useState(-2);
   const [visible, setVisible] = useState(false);
+
+  const likeHander = () => {
+    if (liked == false) {
+      setVisible(true);
+    }
+
+    setLiked(!liked);
+    setCounter(index2);
+  };
+
+  useEffect(() => {
+    if (liked == true) {
+      Animated.spring(currentValue, {
+        toValue: 3,
+        friction: 2,
+        useNativeDriver: true,
+      }).start(() => {
+        Animated.spring(currentValue, {
+          toValue: 1,
+          useNativeDriver: true,
+          friction: 2,
+        }).start(() => {
+          setVisible(false);
+        });
+      });
+    }
+  }, [liked]);
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -27,15 +56,22 @@ export default function ShopHeader({navigation, id}) {
               onPress={() => navigation.goBack()}
             />
           </View>
-
           <View style={styles.view3}>
             <Icon
               name={liked && index2 == counter ? 'favorite' : 'favorite-border'}
               type="material"
               color="red"
               size={30}
+              onPress={likeHander}
             />
           </View>
+        </View>
+        <View style={styles.view4}>
+          {visible && index2 == counter && (
+            <Animated.View style={{transform: [{scale: currentValue}]}}>
+              <Icon name="favorite" size={40} color="red" type="material" />
+            </Animated.View>
+          )}
         </View>
       </ImageBackground>
     </View>
